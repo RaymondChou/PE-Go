5e09e9de7b78819c01e1a3428a118cb64be69685
RaymondChou <freezestart@gmail.com>
2014-03-25 18:08:44 +0800

    more

--- cgo.go
+++ cgo.go
@@ -2,26 +2,39 @@ package main
 
 /*
 #cgo CFLAGS: -I/usr/include/cups
-#cgo LDFLAGS: -lcups
-#include </usr/include/cups/cups.h>
-#include <stdlib.h>
+#cgo LDFLAGS: -lcups -D_IPP_PRIVATE_STRUCTURES
+#include <cups.h>
 */
 import "C"
+
 import "fmt"
-import "unsafe"
+
+import _ "unsafe"
 
 func main() {
-	// fmt.Println(int(C.random()))
 
 	fmt.Println(C.GoString(C.cupsUser()))
 
 	fmt.Println(C.GoString(C.cupsGetDefault()))
 
-	var options C.cups_option_t
+	var options *C.cups_option_t
 
-	options.name = C.CString("PageSize")
-	options.value = C.CString("Postcard")
+	num_options := C.int(0)
 
-	fmt.Println(options)
-	fmt.Println(C.cupsPrintFile(C.CString("EPSON_Epson_Stylus_Photo_T50"), C.CString("/Users/Raymond/Downloads/hz.png"), C.CString("Test Print"), C.int(0), unsafe.Pointer(options)))
+	num_options = C.cupsAddOption(C.CString("PageSize"), C.CString("Postcard"), C.int(num_options), &options)
+
+	num_options = C.cupsAddOption(C.CString("Media"), C.CString("Photo"), C.int(num_options), &options)
+
+	dest := C.CString("EPSON_Epson_Stylus_Photo_T50")
+	file := C.CString("/Users/Raymond/Downloads/hz.png")
+	title := C.CString("Test Print")
+
+	job_id, err := C.cupsPrintFile(dest, file, title, num_options, options)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(job_id)
+
+	defer C.cupsFreeOptions(num_options, options)
+
 }
